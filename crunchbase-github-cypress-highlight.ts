4d1a465334a9baa2f7f5285a162745329d410216
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2020-04-28 23:01:08 -0700

    [cb] github cypress highlight: include css

--- crunchbase-github-cypress-highlight.ts
+++ crunchbase-github-cypress-highlight.ts
@@ -11,21 +11,61 @@
 (function () {
   "use strict";
 
-  setInterval(() => {
-    const allCypressElements = Array.from(
-      document.querySelectorAll(".js-timeline-item")
-    )
-      .filter((el) => el instanceof HTMLElement)
-      .map((el) => el)
-      .filter((el) => el.innerText.includes("Test summary"));
-    allCypressElements.forEach((cypressElement) => {
-      const failed = cypressElement.innerText.includes("Failures");
-      if (failed) {
-        cypressElement.classList.add("pandu-userscript-ci-failure");
-      } else {
-        cypressElement.classList.add("pandu-userscript-ci-success");
+  function main() {
+    addCSS();
+    addCypressFinder();
+  }
+
+  function addCSS() {
+    const css = (x) => x;
+    var myCss = css`
+      .pandu-userscript-ci-failure p:first-of-type {
+        background: #bb2929;
       }
-      cypressElement.classList.add("pandu-userscript-ci-result");
-    });
-  }, 1000);
+
+      .pandu-userscript-ci-success p:first-of-type {
+        background: #67c05c;
+      }
+
+      .pandu-userscript-ci-result p:first-of-type img {
+        filter: brightness(99);
+      }
+
+      .pandu-userscript-ci-result p:first-of-type {
+        margin-bottom: -12px; /* Set to -16px if jitter becomes a problem */
+        padding-bottom: 16px;
+      }
+
+      .pandu-userscript-ci-result p:first-of-type img {
+        background: transparent;
+      }
+    `;
+    var head = document.head || document.getElementsByTagName("head")[0];
+    var style = document.createElement("style");
+    head.appendChild(style);
+    style.type = "text/css";
+    style.appendChild(document.createTextNode(myCss));
+  }
+
+  function addCypressFinder() {
+    setInterval(() => {
+      const allCypressElements = Array.from(
+        document.querySelectorAll(".js-timeline-item")
+      )
+        .filter((el) => el instanceof HTMLElement)
+        .map((el) => el)
+        .filter((el) => el.innerText.includes("Test summary"));
+      allCypressElements.forEach((cypressElement) => {
+        const failed = cypressElement.innerText.includes("Failures");
+        if (failed) {
+          cypressElement.classList.add("pandu-userscript-ci-failure");
+        } else {
+          cypressElement.classList.add("pandu-userscript-ci-success");
+        }
+        cypressElement.classList.add("pandu-userscript-ci-result");
+      });
+    }, 1000);
+  }
+
+  main();
 })();
