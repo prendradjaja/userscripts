ea8d95ce959478dd10022008cecf8bc23de66cf0
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2020-06-13 14:03:40 -0700

    ++

--- google-calendar-fifteeners.js
+++ google-calendar-fifteeners.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         New Userscript
+// @name         Google Calendar: Fifteen-minute events and other goodies
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
@@ -9,26 +9,55 @@
 // ==/UserScript==
 "use strict";
 
-// TODO naming: differentiate classes and selectors
-const CALENDAR_EVENT = ".EfQccc";
-const TEXT_DESCRIPTION = ".ynRLnc";
+// Main features:
+// - Fifteen-minute events are super-narrow (instead of being as wide as a thirty-minute event)
+// - Resize handles on thirty-minute events are made narrower
 
+// TODO naming: differentiate classes and selectors
+// TODO
+const CALENDAR_EVENT = ".EfQccc";
+const VISIBLE_TEXT_SUMMARY = ".RIOtYe";
+const A11Y_TEXT_SUMMARY = ".ynRLnc";
+const RESIZE_HANDLE = ".leOeGd";
+
+const PBR_THIRTY = "pbr-thirty";
 const PBR_FIFTEENER = "pbr-fifteener";
+
+// Other unused classes:
+// EfQccc: Events including all-day events
 
 function main() {
   addCSS();
-  addFifteenerFinder();
+  addFinders();
 }
 
 function addCSS() {
   var myCss = `
       .${PBR_FIFTEENER} {
-        height: 8px !important;
+        height: 9px !important;
       }
 
-      .pbr-fifteener .RIOtYe {
-        margin-top: -6px;
-        font-size: 10px;
+      .pbr-fifteener ${VISIBLE_TEXT_SUMMARY} {
+        margin-top: -7px;
+        margin-left: -5px;
+        font-size: 11px;
+      }
+
+      .pbr-fifteener.pbr-right-half {
+        left: 20% !important;
+        width: 80% !important;
+      }
+
+      .pbr-fifteener ${RESIZE_HANDLE} {
+        height: 4px; /* 4px is default */
+      }
+
+      .pbr-thirty ${RESIZE_HANDLE} {
+        height: 4px; /* 8px is default */
+      }
+
+      ${RESIZE_HANDLE}:hover {
+        background: rgba(255, 255, 255, 0.5);
       }
     `;
   var head = document.head || document.getElementsByTagName("head")[0];
@@ -38,18 +67,26 @@ function addCSS() {
   style.appendChild(document.createTextNode(myCss));
 }
 
-function addFifteenerFinder() {
+function addFinders() {
   setInterval(() => {
     const allCalendarEvents = Array.from(
       document.querySelectorAll(CALENDAR_EVENT)
     );
 
     allCalendarEvents.forEach((calendarEvent) => {
-      const textDescription = calendarEvent.querySelector(TEXT_DESCRIPTION)
+      const textDescription = calendarEvent.querySelector(A11Y_TEXT_SUMMARY)
         .innerText;
       const duration = getDuration(textDescription);
-      if (duration === 15) {
+      if (duration <= 15) {
         calendarEvent.classList.add(PBR_FIFTEENER);
+        if (
+          calendarEvent.style.left === "50%" &&
+          calendarEvent.style.width === "50%"
+        ) {
+          calendarEvent.classList.add("pbr-right-half");
+        }
+      } else if (duration <= 30) {
+        calendarEvent.classList.add(PBR_THIRTY);
       }
     });
   }, 1000);
