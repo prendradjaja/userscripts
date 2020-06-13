40ac8d19d34c0c36e9d7f8bd468d578471b53fab
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2020-06-13 13:20:10 -0700

    Prettier

--- google-calendar-fifteeners.js
+++ google-calendar-fifteeners.js
@@ -1,8 +1,8 @@
 // TODO naming: differentiate classes and selectors
-const CALENDAR_EVENT = '.EfQccc';
-const TEXT_DESCRIPTION = '.ynRLnc';
+const CALENDAR_EVENT = ".EfQccc";
+const TEXT_DESCRIPTION = ".ynRLnc";
 
-const PBR_FIFTEENER = 'pbr-fifteener';
+const PBR_FIFTEENER = "pbr-fifteener";
 
 function main() {
   addCSS();
@@ -31,10 +31,11 @@ function resizeFifteeners() {
   setInterval(() => {
     const allCalendarEvents = Array.from(
       document.querySelectorAll(CALENDAR_EVENT)
-    )
+    );
 
     allCalendarEvents.forEach((calendarEvent) => {
-      const textDescription = calendarEvent.querySelector(TEXT_DESCRIPTION).innerText;
+      const textDescription = calendarEvent.querySelector(TEXT_DESCRIPTION)
+        .innerText;
       const duration = getDuration(textDescription);
       if (duration === 15) {
         calendarEvent.classList.add(PBR_FIFTEENER);
@@ -46,7 +47,7 @@ function resizeFifteeners() {
 function getDuration(textDescription) {
   const matches = textDescription.match(/\d+(:\d+)?(am|pm)/g);
   if (!matches) {
-    console.warn('No matches for', textDescription);
+    console.warn("No matches for", textDescription);
     return 0;
   }
   const [start, end] = matches.map(parseTime);
@@ -57,22 +58,22 @@ function getDuration(textDescription) {
  * Return minutes from 0:00
  */
 function parseTime(t) {
-  const pm = t.includes('pm');
+  const pm = t.includes("pm");
   let hours, minutes;
-  if (t.includes(':')) {
+  if (t.includes(":")) {
     [hours, minutes] = t
-      .slice(0, -2)
-      .split(':')
+      .slice(0, -2) //
+      .split(":")
       .map(Number);
   } else {
     hours = +t.slice(0, -2);
     minutes = 0;
   }
   return (
-    hours * 60
-    + minutes
-    + (pm ? 12*60 : 0)
-    + (hours === 12 ? -12*60 : 0) // Account for 12pm and 12am
+    hours * 60 + //
+    minutes +
+    (pm ? 12 * 60 : 0) +
+    (hours === 12 ? -12 * 60 : 0) // Account for 12pm and 12am
   );
 }
 
